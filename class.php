3c2bea65f2068141fc3d1ef293e45cfb806cdc07
Erik <erikdmitchell@gmail.com>
2017-12-02 13:03:26 -0600

    added class

---
+++ class.php
@@ -0,0 +1,13 @@
+<?php
+    
+class CI_Demo {
+    
+    public function __construct() {
+
+    }
+    
+    public function make_awesome($string='') {
+        return $string.' AWESOME';    
+    }   
+     
+}
