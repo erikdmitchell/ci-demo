d7db050937bab4d6af06dbc0fb2c2f282ec4cd61
Erik <erikdmitchell@gmail.com>
2017-12-06 08:42:52 -0500

    cleaned up code, added proper spacing

--- src/class-first-class.php
+++ src/class-first-class.php
@@ -3,7 +3,14 @@
 namespace erikdmitchell\ci_demo;
 
 /**
- * First_Class class.
+ * First Class.
+ *
+ * First Class.
+ *
+ * @class    First_Class
+ * @package  
+ * @category Class
+ * @author   
  */
 class First_Class {
 
@@ -11,10 +18,10 @@ class First_Class {
      * Encode a string
      * 
      * @access public
-     * @param mixed $string
+     * @param string $string (default: '')
      * @return void
      */
-    public function encode_string($string) {
+    public function encode_string( $string='' ) {
         $string = strtolower($string);
         $src = "abcdefghijklmnopqrstuvwxyz0123456789";
         $dst = "jklmnopqrstuvwxyz0123456789abcdefghi";
@@ -22,7 +29,7 @@ class First_Class {
         for ($i = 0; $i < strlen($string); $i++) {
             $pos = strpos($src, $string[$i]);
             if ($pos === false) {
-                throw new \Exception("Please provide only numbers and alphanumerical characters");
+                return "Please provide only numbers and alphanumerical characters";
             }
             $string[$i] = $dst[$pos];
         }
@@ -34,20 +41,21 @@ class First_Class {
      * Decode a string
      * 
      * @access public
-     * @param mixed $string
+     * @param mixed $string (default: '')
      * @return void
      */
-    public function decode_string($string) {
-        $string=strtolower($string);
-        $src="jklmnopqrstuvwxyz0123456789abcdefghi ";
-        $dst="abcdefghijklmnopqrstuvwxyz0123456789 ";
-        for ($i=0; $i<strlen($string); $i++) {
-            $pos=strpos($src, $string[$i]);
-            if ($pos===false) {
-                throw new \Exception("Please provide only numbers and alphanumerical characters");
+    public function decode_string( $string='' ) {
+        $string = strtolower($string);
+        $src = "jklmnopqrstuvwxyz0123456789abcdefghi ";
+        $dst = "abcdefghijklmnopqrstuvwxyz0123456789 ";
+        for ($i = 0; $i < strlen($string); $i++) {
+            $pos = strpos($src, $string[$i]);
+            if ($pos === false) {
+                return "Please provide only numbers and alphanumerical characters";
             }
-            $string[$i]=$dst[$pos];
+            $string[$i] = $dst[$pos];
         }
+        
         return $string;
     }
 }
