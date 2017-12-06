20853153295e1434f478c79dcb1e5a186dfbe794
Erik <erikdmitchell@gmail.com>
2017-12-06 08:49:42 -0500

    code fixed via phpcbf

--- src/class-first-class.php
+++ src/class-first-class.php
@@ -5,57 +5,55 @@ namespace erikdmitchell\ci_demo;
 /**
  * First Class.
  *
- * First Class.
- *
  * @class    First_Class
- * @package  
+ * @package
  * @category Class
- * @author   
+ * @author
  */
 class First_Class {
 
     /**
      * Encode a string
-     * 
+     *
      * @access public
      * @param string $string (default: '')
      * @return void
      */
-    public function encode_string( $string='' ) {
-        $string = strtolower($string);
-        $src = "abcdefghijklmnopqrstuvwxyz0123456789";
-        $dst = "jklmnopqrstuvwxyz0123456789abcdefghi";
-        
-        for ($i = 0; $i < strlen($string); $i++) {
-            $pos = strpos($src, $string[$i]);
-            if ($pos === false) {
-                return "Please provide only numbers and alphanumerical characters";
+    public function encode_string( $string = '' ) {
+        $string = strtolower( $string );
+        $src    = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        $dst    = 'jklmnopqrstuvwxyz0123456789abcdefghi';
+
+        for ( $i = 0; $i < strlen( $string ); $i++ ) {
+            $pos = strpos( $src, $string[ $i ] );
+            if ( $pos === false ) {
+                return 'Please provide only numbers and alphanumerical characters';
             }
-            $string[$i] = $dst[$pos];
+            $string[ $i ] = $dst[ $pos ];
         }
-        
+
         return $string;
     }
 
     /**
      * Decode a string
-     * 
+     *
      * @access public
      * @param mixed $string (default: '')
      * @return void
      */
-    public function decode_string( $string='' ) {
-        $string = strtolower($string);
-        $src = "jklmnopqrstuvwxyz0123456789abcdefghi ";
-        $dst = "abcdefghijklmnopqrstuvwxyz0123456789 ";
-        for ($i = 0; $i < strlen($string); $i++) {
-            $pos = strpos($src, $string[$i]);
-            if ($pos === false) {
-                return "Please provide only numbers and alphanumerical characters";
+    public function decode_string( $string = '' ) {
+        $string = strtolower( $string );
+        $src    = 'jklmnopqrstuvwxyz0123456789abcdefghi ';
+        $dst    = 'abcdefghijklmnopqrstuvwxyz0123456789 ';
+        for ( $i = 0; $i < strlen( $string ); $i++ ) {
+            $pos = strpos( $src, $string[ $i ] );
+            if ( $pos === false ) {
+                return 'Please provide only numbers and alphanumerical characters';
             }
-            $string[$i] = $dst[$pos];
+            $string[ $i ] = $dst[ $pos ];
         }
-        
+
         return $string;
     }
 }
