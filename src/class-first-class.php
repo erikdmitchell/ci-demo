32f65b7a4780933948844f948ca092d8a6747209
Erik <erikdmitchell@gmail.com>
2017-12-06 08:58:47 -0500

    first class is now clean and meets standards

--- src/class-first-class.php
+++ src/class-first-class.php
@@ -16,15 +16,16 @@ class First_Class {
      * Encode a string
      *
      * @access public
-     * @param string $string (default: '')
-     * @return void
+     * @param string $string the string to encode.
+     * @return string
      */
     public function encode_string( $string = '' ) {
         $string = strtolower( $string );
         $src    = 'abcdefghijklmnopqrstuvwxyz0123456789';
         $dst    = 'jklmnopqrstuvwxyz0123456789abcdefghi';
+        $length = strlen( $string );
 
-        for ( $i = 0; $i < strlen( $string ); $i++ ) {
+        for ( $i = 0; $i < $legnth; $i++ ) {
             $pos = strpos( $src, $string[ $i ] );
             if ( false === $pos ) {
                 return 'Please provide only numbers and alphanumerical characters';
@@ -39,15 +40,16 @@ class First_Class {
      * Decode a string
      *
      * @access public
-     * @param mixed $string (default: '')
-     * @return void
+     * @param mixed $string the string to decode.
+     * @return string
      */
     public function decode_string( $string = '' ) {
         $string = strtolower( $string );
         $src    = 'jklmnopqrstuvwxyz0123456789abcdefghi ';
         $dst    = 'abcdefghijklmnopqrstuvwxyz0123456789 ';
-        
-        for ( $i = 0; $i < strlen( $string ); $i++ ) {
+        $length = strlen( $string );
+
+        for ( $i = 0; $i < $length; $i++ ) {
             $pos = strpos( $src, $string[ $i ] );
             if ( false === $pos ) {
                 return 'Please provide only numbers and alphanumerical characters';
